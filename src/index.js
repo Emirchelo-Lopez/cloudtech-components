9769a7ae9905f05ddeb527bb8bc7c51407cd6923
Emirchelo Lopez <elopezarmen@gmail.com>
2024-05-27 12:57:25 -0600

    Stopped tracking .cache and dropdown menu added

--- src/index.js
+++ src/index.js
@@ -19,3 +19,21 @@ minusButton.addEventListener("click", function () {
     counter.innerHTML = counterInt;
   }
 });
+
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = option.innerText;
+    sBtn_text.innerText = selectedOption;
+
+    optionMenu.classList.remove("active");
+  });
+});
